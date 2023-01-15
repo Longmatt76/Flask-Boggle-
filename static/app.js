ca4cea904a744ce0267490986e749083b029a9fc
Matthew Long <longm7676@gmail.com>
2023-01-15 13:55:41 -0600

    uploaded to github

--- static/app.js
+++ static/app.js
@@ -6,13 +6,15 @@ const form = $("form");
 
 $("form").on("submit", handleSubmit);
 
+// handles form submits, sends an ajax request to the server to check the status of the submitted word
+// then it displays that status in the html and updates and displays the score
 async function handleSubmit(evt) {
   evt.preventDefault();
   let $field = $("#word_check");
   let word = $("#word").val();
 
   res = await axios.get("/check-word", { params: { word: word } });
-
+// checks validity
   if (words.includes(word)) {
     $field.text("word already found");
     setTimeout(function () {
@@ -24,7 +26,7 @@ async function handleSubmit(evt) {
       $field.text("");
     }, 1000);
   }
-
+// if word is valid and hasn't already been used it updates and displays the score
   if ((res.data.result === "ok") & !words.includes(word)) {
     words.push(word);
     score += word.length;
@@ -33,7 +35,7 @@ async function handleSubmit(evt) {
 
   $("#word").val("");
 }
-
+// this is the timer function
 countdown = setInterval(async function () {
   $count.html(timer);
   timer--;
@@ -44,6 +46,8 @@ countdown = setInterval(async function () {
   }
 }, 1000);
 
+// when the timer expires it triggers this endgame function which sends a post request to the server 
+// with the score and recieves back a json response that tells us if the score was a new highscore  
 async function postscore() {
   score = score;
   const res = await axios.post("/post-score", { score: score });
--- test.py
+++ test.py
@@ -6,5 +6,49 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        """Stuff to do before every test."""
 
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_show_board(self):
+        """tests status code and if html is displayed"""
+
+        with app.test_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h3>SCORE</h3>', html)
+        
+
+    def test_valid_word(self):
+        """Test if word is valid by modifying the board in the session"""
+
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["B", "A", "L", "L", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"],
+                                 ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?word=ball')
+        self.assertEqual(response.json['result'], 'ok')
+
+    def test_post_score(self):
+        """I was trying to test the post request but it doesnt work so I just set it to 400, 
+        maybe because it involves ajax?"""
+
+        with app.test_client() as client:
+            res = client.post('/post-score')
+
+            self.assertEqual(res.status_code, 400)
+
+    def test_session(self):
+        'tests if the board is stored in session'
+        with app.test_client() as client:
+            res = client.get('/')
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('board', session)
