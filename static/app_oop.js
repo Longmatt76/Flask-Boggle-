de23aac161a492cc73a22d45b8bce80aa0db6b19
Matthew Long <longm7676@gmail.com>
2023-01-17 15:52:59 -0600

    oop fix

--- static/app_oop.js
+++ static/app_oop.js
@@ -4,18 +4,41 @@ class BoggleGame {
     this.score = 0;
     this.words = [];
 
-    $("form").on("submit", this.handleSubmit);
-    
+    $("form").on("submit", this.handleSubmit.bind(this));
+
+    // this is the timer function
+    this.countdown = setInterval(async () => {
+      const $count = $("#count");
+      $count.html(this.timer);
+      this.timer--;
+      if (this.timer === -1) {
+        clearInterval(this.countdown);
+        $("form").hide();
+        await this.postscore();
+      }
+    }, 1000);
   }
-// handles form submits, sends an ajax request to the server to check the status of the submitted word
-// then it displays that status in the html and updates and displays the score
+
+  // when the timer expires it triggers this endgame function which sends a post request to the server
+  // with the score and recieves back a json response that tells us if the score was a new highscore
+  async postscore() {
+    const res = await axios.post("/post-score", { score: this.score });
+    if (res.data.brokeRecord) {
+      alert(`Congrats, you set a new high score of ${this.score}`);
+    } else {
+      alert(`Time is up, you scored ${this.score}`);
+    }
+  }
+
+  // handles form submits, sends an ajax request to the server to check the status of the submitted word
+  // then it displays that status in the html and updates and displays the score
   async handleSubmit(evt) {
     evt.preventDefault();
     let $field = $("#word_check");
     let word = $("#word").val();
 
-     const res = await axios.get("/check-word", { params: { word: word } });
-// checks validity
+    const res = await axios.get("/check-word", { params: { word: word } });
+    // checks validity
     if (this.words.includes(word)) {
       $field.text("word already found");
       setTimeout(function () {
@@ -27,34 +50,13 @@ class BoggleGame {
         $field.text("");
       }, 1000);
     }
-// if word is valid and hasn't already been used it updates and displays the score
+    // if word is valid and hasn't already been used it updates and displays the score
     if ((res.data.result === "ok") & !this.words.includes(word)) {
       this.words.push(word);
       this.score += word.length;
-      $("#postscore").text(score);
+      $("#postscore").text(this.score);
     }
 
     $("#word").val("");
   }
-// when the timer expires it triggers this endgame function which sends a post request to the server 
-// with the score and recieves back a json response that tells us if the score was a new highscore  
-  async postscore() {
-    const res = await axios.post("/post-score", { score: this.score });
-    if (res.data.brokeRecord) {
-      alert(`Congrats, you set a new high score of ${score}`);
-    } else {
-      alert(`Time is up, you scored ${score}`);
-    }
-  }
 }
-// this is the timer function
-countdown = setInterval(async function () {
-  const $count = $("#count");
-  $count.html(this.timer);
-  this.timer--;
-  if (this.timer === -1) {
-    clearInterval(countdown);
-    $("form").hide();
-    await this.postscore();
-  }
-}, 1000);
